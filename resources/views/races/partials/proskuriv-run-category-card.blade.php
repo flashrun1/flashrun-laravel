7603aa8c50c72b7ef441455c52a7a1f40a4c1c18
vitalii korolchuk <vitalik.kor@gmail.com>
2024-02-14 00:54:20 +0200

    proskuriv run tweaks

--- resources/views/races/partials/proskuriv-run-category-card.blade.php
+++ resources/views/races/partials/proskuriv-run-category-card.blade.php
@@ -8,8 +8,9 @@
             </div>
             <div class="col-sm-7">
                 <h3 class="title" style="text-transform: none;">ПроскурівRUN</h3>
-                <div class="descr custom-color">Весняний крос, який відбудеться 23 квітня 2023 року в дендропарку.</div>
+                <div class="descr custom-color">Весняний крос, який відбудеться 21 квітня 2024 року в дендропарку.</div>
                 <button type="button" class="btn" data-toggle="modal" data-target="#registr_modal_proskuriv_run_2024">ЗАРЕЄСТРУВАТИСЬ</button>
+                <a class="mt-2 text-decoration-underline" href="{{ route('race-participants', ['raceId' => \App\Models\Race::getIdBySlug('proskuriv-run-2024')]) }}">Список учасників</a>
             </div>
         </div>
     </div>
